d072ef184a1e6db75688c67dd8b46ecdf944c262
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2024-08-29 08:43:40 +0700

    feat(components): enhance Form and Navbar components
    - Form.vue:
      - Add class prop for dynamic styling
      - Implement script setup with defineProps
    
    - Navbar.vue:
      - Add navigation links using RouterLink
      - Implement active and hover styles for links
      - Import RouterLink from vue-router

--- vue-frontend/src/components/Navbar.vue
+++ vue-frontend/src/components/Navbar.vue
@@ -1,8 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <nav class="flex bg-green-400 h-10 items-center">
-    <ul class="ml-20 text-white">
-      <li>Home</li>
+    <ul class="ml-20 text-white flex gap-10">
+      <RouterLink
+        to="/"
+        class="hover:border-b-4 hover:border-green-500 p-1 rounded-sm"
+        active-class="border-b-4 border-white"
+        >Home</RouterLink
+      >
+      <RouterLink
+        to="/about"
+        class="hover:border-b-4 hover:border-green-500 p-1 rounded-sm"
+        active-class="border-b-4 border-white"
+        >About</RouterLink
+      >
+      <RouterLink to="/login" class="hover:border-b-4 hover:border-green-500 p-1 rounded-sm"
+        >Logout</RouterLink
+      >
     </ul>
   </nav>
 </template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
