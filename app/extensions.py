b3b2e14bc824059b985a332deb876e971269bc5e
Akhmad Taufik Ismail <akhmadtaufik705@gmail.com>
2024-08-30 17:34:25 +0700

    style(codebase): improve readability across multiple modules

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager # ditambah
+from flask_jwt_extended import JWTManager  # ditambah
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 migrate = Migrate()
